2887b8228deb2a98670e9e8789b6deaea6d8e048
Vilkova Viktoria <vilkova_viktoria2000@mail.ru>
2021-08-21 19:09:39 +0500

    Initial commit

---
+++ app/src/main/java/com/example/count/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.count;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    Button addNumberButton;
+
+    public int number = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultField = findViewById(R.id.result);
+        addNumberButton = findViewById(R.id.addNumber);
+
+        // приложение запущено впервые => счётчик обнуляется
+        if(savedInstanceState == null) {
+            number = 0;
+        }
+
+        // обработчик нажатия на кнопку
+        addNumberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNumber();
+            }
+        });
+    }
+
+    public void addNumber() {
+        number += 1;
+        resultField.setText(String.valueOf(number));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        number = savedInstanceState.getInt("number");
+        resultField.setText(String.valueOf(number));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("number", number);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
+
